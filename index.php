51d92f7ca074d71656cbba3e2b7b80d9c4d26d99
IZERE HIRWA Roger <53377810+IZEREROGER@users.noreply.github.com>
2021-11-23 16:33:36 +0200

    red index

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<center>
+  <i><h1>Contacting Rogers Hdev servers to serv application</h1></i>
+  <i>Redirecting...</i>
+</center>
+<script>
+  window.location.href = 'http://app-0237bc2b-c709-4778-81a2-57cd95240b7b.cleverapps.io/';
+</script>
+</html>
